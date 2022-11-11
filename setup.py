88e8109899b1cb6d9fa0dcd78277f2c439257396
David Kuda <david@kuda.ai>
2022-11-11 21:25:51 +0100

    remove email

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version="0.1",
     description="Config mgt to connect to dbs.",
     author="David Kuda",
-    author_email="davidkuda3@gmail.com",
     packages=find_packages("src"),
     package_dir={"": "src"},
 )
