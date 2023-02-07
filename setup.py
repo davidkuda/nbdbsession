a9f1614940f71c0e2208fa9d1e657e6570a81831
davidkuda <david@kuda.ai>
2023-02-07 22:48:03 +0100

    update setup.py
    Parse README.md and remove underscores
    from name.

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from distutils.core import setup
+from pathlib import Path
 from setuptools import find_packages
 
-# nb_db_session := notebook_database_session
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+# nbdbsession := notebook_database_session
 setup(
-    name="nb_db_session",
-    version="0.1",
+    name="nbdbsession",
+    version="0.1.1",
     description="Config mgt to connect to databases from jupyter notebooks.",
     author="David Kuda",
     license="MIT",
     packages=find_packages("src"),
     package_dir={"": "src"},
     url="https://github.com/davidkuda/jupyter_database_io",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
