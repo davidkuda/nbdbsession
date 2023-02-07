7929dcbcfd20c46f4392b16ee89b5bfa852d63bd
davidkuda <david@kuda.ai>
2023-02-07 22:23:05 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+# nb_db_session := notebook_database_session
 setup(
-    name="db_connector",
+    name="nb_db_session",
     version="0.1",
-    description="Config mgt to connect to dbs.",
+    description="Config mgt to connect to databases from jupyter notebooks.",
     author="David Kuda",
+    license="MIT",
     packages=find_packages("src"),
     package_dir={"": "src"},
+    url="https://github.com/davidkuda/jupyter_database_io",
 )
